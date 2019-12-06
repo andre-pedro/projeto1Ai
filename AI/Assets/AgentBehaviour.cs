3f2ac4eabca126201fc98a0fb833777a487e4b35
IssaMaia <Diogo_maia99@hotmail.com>
2019-12-06 16:55:12 +0000

    Food and Tired Stats and searches for food

--- AI/Assets/AgentBehaviour.cs
+++ AI/Assets/AgentBehaviour.cs
@@ -6,20 +6,38 @@ using UnityEngine.AI;
 public class AgentBehaviour : MonoBehaviour
 {
     public Behaviour behaviour;
+
     public bool isAlive;
     public bool isStunned;
     public bool inPanic;
+    public bool isHungry;
+    public bool isTired;
+
     private NavMeshAgent agent;
-    private Vector3 destination;
     private NavMeshPath path;
+
+    private Vector3 destination;    
     private Vector3 panicOrigin;
+
     private Transform exit;
+    private Transform restauracao;
+    private Transform openArea;
+
+    private float hunger;
+    private float tired;
+    
 
     private void Start()
     {
+        hunger = Random.Range(50f, 101f);
+        tired = Random.Range(50f, 101f);
+
         path = new NavMeshPath();
         agent = GetComponent<NavMeshAgent>();
+
         exit = GameObject.FindGameObjectWithTag("Exit").transform;
+        restauracao = GameObject.FindGameObjectWithTag("Eat").transform;
+        openArea = GameObject.FindGameObjectWithTag("Open").transform;
 
         behaviour = Behaviour.Wander;
 
@@ -28,6 +46,24 @@ public class AgentBehaviour : MonoBehaviour
 
     private void Update()
     {
+
+        if(hunger >= 0f)
+        {
+            hunger -= Random.Range(0f, 0.03f);
+
+        }else if (hunger < 0f)
+        {
+            hunger = 0f;
+        }
+        if (tired >= 0f)
+        {
+            tired -= Random.Range(0f, 0.04f);
+
+        }else if(tired < 0f)
+        {
+            tired = 0f;
+        }                   
+        
         switch (behaviour)
         {
             case Behaviour.Idle:
@@ -35,22 +71,50 @@ public class AgentBehaviour : MonoBehaviour
                 break;
 
             case Behaviour.Wander:
+                if (hunger <= 25f)
+                {
+                    isHungry = true;
+                    behaviour = Behaviour.Seek;                        
+                }
+                if (tired <= 25f)
+                {
+                    isTired = true;
+                    behaviour = Behaviour.Seek;
+                }
+                
                 Wander();
+                                   
                 break;
 
             case Behaviour.Seek:
-                //Seek();
+                //Prioritizes Food over Tired
+                if (isHungry)
+                {
+                    SeekFood();
+
+                }else if (isTired)
+                {
+
+                }
+                
                 break;
 
             case Behaviour.Flee:
                 if (agent.remainingDistance < 0.5f) Debug.Log("DONE");
                 break;
-        }
+        }       
+
+        
     }
 
     private void Idle()
     {
         agent.isStopped = true;
+    }
+
+    private void SeekFood()
+    {
+        StartCoroutine(GoToFood());
     }
 
     private void Wander()
@@ -105,6 +169,11 @@ public class AgentBehaviour : MonoBehaviour
             GenerateWanderDestination();
     }
 
+    private void GenerateSeekDestination()
+    {
+        Vector3 linear = Vector3.zero;
+    }
+
     private void GenerateFleeDestination(Vector3 panicOrigin)
     {
         Vector3 linear = Vector3.zero;
@@ -126,6 +195,15 @@ public class AgentBehaviour : MonoBehaviour
             isStunned = false;
             agent.isStopped = false;
             Panic(panicOrigin);
+        }
+    }
+
+    private IEnumerator GoToFood()
+    {
+        yield return new WaitForSeconds(Random.Range(5, 10));
+        if (isHungry)
+        {
+            agent.SetDestination(restauracao.position);
         }
     }
 
